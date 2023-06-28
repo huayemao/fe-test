da6a8f4e1d6907d9f08f52208dee2dd3688b6e37
huayemao <huayemao4g@gmail.com>
2023-06-28 17:59:57 +0800

    fix: 补充遗漏提交的常量文件

---
+++ constants.ts
@@ -0,0 +1,37 @@
+export interface SlideItem {
+  image: string;
+  caption: string;
+}
+
+export const slides: SlideItem[] = [
+  {
+    image:
+      "https://www.peugeot.com.cn/Cg/Upload/www.peugeot.com.cn/image/230529/2023052917174454808.png",
+    caption: "Slide 1",
+  },
+  {
+    image:
+      "https://www.peugeot.com.cn/Cg/Upload/www.peugeot.com.cn/image/230131/2023013111031915986.jpg",
+    caption: "Slide 2",
+  },
+  {
+    image:
+      "https://www.peugeot.com.cn/Cg/Upload/www.peugeot.com.cn/image/230428/2023042822591769064.jpg",
+    caption: "Slide 3",
+  },
+  {
+    image:
+      "https://www.peugeot.com.cn/Cg/Upload/www.peugeot.com.cn/image/230428/2023042822591769064.jpg",
+    caption: "Slide 3",
+  },
+  {
+    image:
+      "https://www.peugeot.com.cn/Cg/Upload/www.peugeot.com.cn/image/230428/2023042822591769064.jpg",
+    caption: "Slide 3",
+  },
+  {
+    image:
+      "https://www.peugeot.com.cn/Cg/Upload/www.peugeot.com.cn/image/230428/2023042822591769064.jpg",
+    caption: "Slide 3",
+  },
+];
